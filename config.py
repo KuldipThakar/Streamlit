cd2f61431db98eda91009cb69be8a78229ca56db
KuldipThakar <kuldip.thakar@bpl.in>
2025-09-02 11:46:10 +0530

    changed the location

--- config.py
+++ config.py
@@ -9,10 +9,11 @@ PROJECT_DATA_DIR = "data/"
 
 # Predefined project files
 
+url = "https://bplmedtech-my.sharepoint.com/:x:/g/personal/kuldip_thakar_bpl_in/EUQLMHc4tqJCnjZN7Yri-yoBGu7Cg5zz-k4mT-0BNzYQOQ?e=su4M2n"
 
 PROJECT_FILES = {
 
-    "CView FD": os.path.join("E:\\OneDrive - BPL Medical Technologies Pvt. Ltd\Exports\\eeee.xlsx"),
+    "CView FD": os.path.join(url),
     "VIVID View ": os.path.join(PROJECT_DATA_DIR, "data.xlsx"),
     "Patient Monitor": os.path.join(PROJECT_DATA_DIR, "1.csv"),
     "ECG": os.path.join(PROJECT_DATA_DIR, "C-ray Pro Plus Cview(II).xlsx"),
