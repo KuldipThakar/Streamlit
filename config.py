e81fa2c3a66a202c1db9a68531c041dd8054fa7c
KuldipThakar <kuldip.thakar@bpl.in>
2025-09-03 09:29:30 +0530

    new added

--- config.py
+++ config.py
@@ -9,11 +9,12 @@ PROJECT_DATA_DIR = "data/"
 
 # Predefined project files
 
-url = "https://bplmedtech-my.sharepoint.com/:x:/g/personal/kuldip_thakar_bpl_in/EUQLMHc4tqJCnjZN7Yri-yoBGu7Cg5zz-k4mT-0BNzYQOQ?e=su4M2n"
+#url = "https://bplmedtech-my.sharepoint.com/:x:/g/personal/kuldip_thakar_bpl_in/EUQLMHc4tqJCnjZN7Yri-yoBaaVuqijjpXgtzHqJ4ighHw?e=77zfrZ"
+file_path = "E:\\OneDrive - BPL Medical Technologies Pvt. Ltd\\Exports\\eeee.xlsx"
 
 PROJECT_FILES = {
 
-    "CView FD": os.path.join(url),
+    "CView FD": file_path,
     "VIVID View ": os.path.join(PROJECT_DATA_DIR, "data.xlsx"),
     "Patient Monitor": os.path.join(PROJECT_DATA_DIR, "1.csv"),
     "ECG": os.path.join(PROJECT_DATA_DIR, "C-ray Pro Plus Cview(II).xlsx"),
